ea6de3e93f4d90c495f537484f96f8b256b06269
freddy3rd <clifford.dle.arnejo@gmail.com>
2026-04-27 12:26:33 +0800

    update

--- src/pages/components/Commodities.tsx
+++ src/pages/components/Commodities.tsx
@@ -159,23 +159,25 @@ useEffect(() => {
         </div>
 
         {/* RIGHT: DYNAMIC DESCRIPTION & FEATURES */}
-        <div className="flex flex-col justify-center p-8 md:p-12 h-full order-1 md:order-2">
+        <div className="flex flex-col justify-center p-8 md:p-12 h-full order-1 md:order-2 flex-col-reverse md:flex-col">
           
-          {/* Dynamic Content: Title */}
-          <h2
-            ref={titleRef} // Hooked up for GSAP fade
-            className="text-4xl md:text-5xl font-display mb-6 tracking-tight text-brand-900"
-          >
-            {COMMODITIES[activeIndex]?.title} {/* Initial state text */}
-          </h2>
+          <div>
+                      {/* Dynamic Content: Title */}
+            <h2
+              ref={titleRef} // Hooked up for GSAP fade
+              className="text-4xl md:text-5xl font-display mb-6 tracking-tight text-brand-900"
+            >
+              {COMMODITIES[activeIndex]?.title} {/* Initial state text */}
+            </h2>
 
-          {/* Dynamic Content: Description */}
-          <p
-            ref={descRef} // Hooked up for GSAP fade
-            className="text-base text-text-secondary max-w-md mb-12 font-body leading-relaxed min-h-50"
-          >
-            {COMMODITIES[activeIndex]?.description} {/* Initial state text */}
-          </p>
+            {/* Dynamic Content: Description */}
+            <p
+              ref={descRef} // Hooked up for GSAP fade
+              className="text-base text-text-secondary max-w-md mb-12 font-body leading-relaxed min-h-50"
+            >
+              {COMMODITIES[activeIndex]?.description} {/* Initial state text */}
+            </p>
+          </div>
 
           {/* Static Content: Features Grid */}
           <div className="grid grid-cols-2 md:grid-cols-4 w-full py-10 border-t border-border-soft gap-y-10 gap-x-6 mt-auto">
